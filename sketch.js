be08ed3430f89b478c4734a2def863fb1629b521
Claire G <claire.gobert2002@gmail.com>
2025-01-17 13:37:10 +0100

    Ajout possibilite de melanger deux concepts

--- sketch.js
+++ sketch.js
@@ -24,7 +24,12 @@ let sugaryColors = [
     '#b8336a', '#c490d1', '#acacde', '#abdafc', '#e5fcff'
 ];
 
-let selectedConcept;
+let selectedConcept1;
+let selectedConcept2;
+
+let buttonCold = document.getElementById('cold');
+let buttonSugary = document.getElementById('sugary');
+let buttonAcid = document.getElementById('acid');
 
 function setup() {
     createCanvas(window.screen.width, 600);
@@ -34,54 +39,91 @@ function setup() {
 function draw() {
     clear();
 
-    if(selectedConcept) {
-        generateColorSets(selectedConcept);
+    selectedConcept1 = localStorage.getItem('selectedConcept1');
+    selectedConcept2 = localStorage.getItem('selectedConcept2');
+
+    if(selectedConcept1 === buttonCold.id || selectedConcept2 === buttonCold.id) {
+        buttonCold.style.textDecoration = "underline";
+    } else {
+        buttonCold.style.textDecoration = "none";
+    }
+    if(selectedConcept1 === buttonSugary.id || selectedConcept2 === buttonSugary.id) {
+        buttonSugary.style.textDecoration = "underline";
+    } else {
+        buttonSugary.style.textDecoration = "none";
+    }
+    if(selectedConcept1 === buttonAcid.id || selectedConcept2 === buttonAcid.id) {
+        buttonAcid.style.textDecoration = "underline";
+    } else {
+        buttonAcid.style.textDecoration = "none";
+    }
+
+    if(selectedConcept1 || selectedConcept2) {
+        generateColorSets(selectedConcept1, selectedConcept2);
     }
     
     drawColorSets();
 }
 
-function generateColorSets(selectedConcept) {
+function generateColorSets(concept1, concept2) {
     for (let j = 0; j < 7; j++) {
-        let colors = generateColors(selectedConcept);
-        colorSets.push({ colors: colors, concept: selectedConcept });
+        let colors = generateColors(concept1, concept2);
+        colorSets.push({ colors: colors, concept1: concept1, concept2: concept2 });
     }
 }
 
-function generateColors(concept) {
+function generateColors(concept1, concept2) {
     let colors = [];
-    let colorSet = [];
+    let colorSet1 = [];
+    let colorSet2 = [];
 
-    switch (concept) {
+    switch (concept1) {
         case 'cold':
-            colorSet = coldColors;
+            colorSet1 = coldColors;
             break;
         case 'sugary':
-            colorSet = sugaryColors;
+            colorSet1 = sugaryColors;
             break;
         case 'acid':
-            colorSet = acidColors;
+            colorSet1 = acidColors;
             break;
         default:
-            colorSet = Array.from({ length: 256 }, (_, i) => color(i, i, i));
+            colorSet1 = Array.from({ length: 256 }, (_, i) => color(i, i, i));
             break;
     }
 
+    if (concept2) {
+        switch (concept2) {
+            case 'cold':
+                colorSet2 = coldColors;
+                break;
+            case 'sugary':
+                colorSet2 = sugaryColors;
+                break;
+            case 'acid':
+                colorSet2 = acidColors;
+                break;
+            default:
+                colorSet2 = Array.from({ length: 256 }, (_, i) => color(i, i, i));
+                break;
+        }
+    }
+
+    let combinedColorSet = concept2 !== "null" ? colorSet1.concat(colorSet2) : colorSet1;
+
     let attempts = 0;
     while (colors.length < 4) {
-        let newColor = random(colorSet);
+        let newColor = random(combinedColorSet);
         let isContrasting = true;
 
-        if (!colors.includes(newColor)) {
-                if(colors.length >= 1) {
-                    if(!hasContrast(colors[colors.length-1], newColor) || attempts > 20) {
-                        isContrasting = false;
-                    }
-                }
-                    
-            if(isContrasting) {
-                colors.push(color(newColor));
+        if(colors.length >= 1) {
+            if(!hasContrast(colors[colors.length-1], newColor) || attempts > 20) {
+                isContrasting = false;
             }
+        }
+
+        if(isContrasting && !colors.includes(newColor)) {
+            colors.push(color(newColor));
         }
     }
 
@@ -101,7 +143,7 @@ function drawColorSets() {
     let startX = (width - totalWidth) / 2; // Calcul de la position de départ pour centrer
 
     let x = startX;
-    drawConcept(selectedConcept, startX);
+    drawConcept(selectedConcept1, selectedConcept2, startX);
     for (let i = 0; i < colorSets.length; i++) {
         stroke(0);
         strokeWeight(3);
@@ -112,12 +154,16 @@ function drawColorSets() {
     }
 }
 
-function drawConcept(concept, x) {
+function drawConcept(concept1, concept2, x) {
     fill(51);
     textSize(32);
     textStyle(BOLD);
     textFont('Verdana');
-    text(`Concept: ${concept}`, x, 50);
+    if(concept2 !== "null"){
+        text(`Concept: ${concept1} and ${concept2}`, x, 50);
+    } else {
+        text(`Concept: ${concept1}`, x, 50);
+    }
 }
 
 function drawColorSet(colors, x, y) {
@@ -146,20 +192,46 @@ function drawColorSetDetails(colors, x, y) {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const params = new URLSearchParams(window.location.search);
-    const concept = params.get('concept');
+function filterColors(concept) {
+    let selectedConcept1 = localStorage.getItem('selectedConcept1');
+    let selectedConcept2 = localStorage.getItem('selectedConcept2');
 
-    if (concept && ['cold', 'sugary', 'acid'].includes(concept)) {
-        filterColors(concept);
+    if (selectedConcept1 === "null") selectedConcept1 = null;
+    if (selectedConcept2 === "null") selectedConcept2 = null;
+
+    console.log('1: ', selectedConcept1)
+    console.log('2: ', selectedConcept2)
+
+    if(selectedConcept1 === null && selectedConcept2 === null) {
+        console.log('1 et 2 == null -> je vais dans 1')
+        localStorage.setItem('selectedConcept1', concept);
+    } else if(selectedConcept1 !== null && selectedConcept2 !== null) {
+        if(selectedConcept1 === concept) {
+            console.log('1 == concept donc je retire 1 et je passe 2 à 1')
+            localStorage.setItem('selectedConcept1', selectedConcept2);
+            localStorage.setItem('selectedConcept2', null);
+        } else if(selectedConcept2 === concept) {
+            console.log('2 == concept donc je retire 2')
+            localStorage.setItem('selectedConcept2', null);
+        } else {
+            console.log('1 et 2 != null et != concept donc je mets 1 à 2 et 2 à concept')
+            localStorage.setItem('selectedConcept1', selectedConcept2);
+            localStorage.setItem('selectedConcept2', concept);
+        }
     } else {
-        console.error('Invalid concept parameter.');
+        if(selectedConcept1 === concept) {
+            localStorage.setItem('selectedConcept1', null);
+        } else {
+            console.log('1 ou 2 == null (normalement 2) -> je vais dans 2')
+            localStorage.setItem('selectedConcept2', concept);
+        }
     }
     
-});
+    colorSets = [];
+    redraw();
+}
 
-function filterColors(concept) {
-    selectedConcept = concept;
+function reload() {
     colorSets = [];
     redraw();
 }
